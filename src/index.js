f36495ad2923b758a69ed98cbd6b563eb7741012
UIT-21522738 <96582896+UIT-21522738@users.noreply.github.com>
2024-05-28 12:51:08 +0700

    port

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const route = require('./routes/index');
 // app.use(helmet())
 
 app.use(cors());
-const port = 3000
+const port =  process.env.PORT || 3000
 app.use(express.static(path.join(__dirname, 'public')));
 
 //middleware
